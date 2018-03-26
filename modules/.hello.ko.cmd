e65c3e272d495ecbf8d02ff6e1d0436f74c3baba
lwei <lwei_uestc@163.com>
2018-03-26 14:33:09 +0800

    不跟踪ko生成过程

--- modules/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/lwei/projects_codeblocks/other_test/modules/hello.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/lwei/projects_codeblocks/other_test/modules/hello.ko /home/lwei/projects_codeblocks/other_test/modules/hello.o /home/lwei/projects_codeblocks/other_test/modules/hello.mod.o ;  true
