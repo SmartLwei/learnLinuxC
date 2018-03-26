0d42d7a8a8418b033c47733c28708f21026cf2ee
lwei <lwei_uestc@163.com>
2018-03-26 11:09:48 +0800

    调通了netlink

--- netlink/kernel/mynlkern.c
+++ netlink/kernel/mynlkern.c
@@ -1,92 +1,103 @@
-    #include <linux/kernel.h>
-    #include <linux/module.h>
-    #include <linux/types.h>
-    #include <linux/sched.h>
-    #include <net/sock.h>
-    #include <linux/netlink.h>
+/*******************************
+file:           k_netlink.c
+description:    netlink demo
+kernel:         linux-3.10.36
+author:         arvik
+email:          1216601195@qq.com
+blog:           http://blog.csdn.net/u012819339
+*******************************/
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/sched.h>
+#include <net/sock.h>
+#include <linux/netlink.h>
 
-    #define NETLINK_TEST 50
-    struct {
-        __u32 pid;
-    }user_process;
+#define NETLINK_USER  22
+#define USER_MSG    (NETLINK_USER + 1)
+#define USER_PORT   50
 
-    static struct sock *netlinkfd = NULL;
+MODULE_LICENSE("GPL");  
+MODULE_AUTHOR("arvik");  
+MODULE_DESCRIPTION("netlink_demo");
 
-    int send_to_user(char *info) //发送到用户空间
+static struct sock *netlinkfd = NULL;
+
+int send_msg(int8_t *pbuf, uint16_t len)
+{
+    struct sk_buff *nl_skb;
+    struct nlmsghdr *nlh;
+
+    int ret;
+
+    nl_skb = nlmsg_new(len, GFP_ATOMIC);
+    if(!nl_skb)
     {
-        int size;
-        struct sk_buff *skb;
-        unsigned char *old_tail;
-        struct nlmsghdr *nlh;  //报文头
-
-        int retval;
-
-        size = NLMSG_SPACE(strlen(info));  //报文大小
-        skb = alloc_skb(size, GFP_ATOMIC); //分配一个新的套接字缓存,使用GFP_ATOMIC标志进程不>会被置为睡眠
-
-        //初始化一个netlink消息首部
-        nlh = nlmsg_put(skb, 0, 0, 0, NLMSG_SPACE(strlen(info))-sizeof(struct nlmsghdr), 0); 
-        old_tail = skb->tail;
-        memcpy(NLMSG_DATA(nlh), info, strlen(info));  //填充数据区
-        nlh->nlmsg_len = skb->tail - old_tail;  //设置消息长度
-
-        //设置控制字段
-        
-        NETLINK_CB(skb).dst_group = 0;
-
-        printk(KERN_DEBUG "[kernel space] skb->data:%s\n", (char *)NLMSG_DATA((struct nlmsghdr *)skb->data));
-
-        //发送数据
-        retval = netlink_unicast(netlinkfd, skb, user_process.pid, MSG_DONTWAIT);
-        printk(KERN_DEBUG "[kernel space] netlink_unicast return: %d\n", retval);
-        return 0;
+        printk("netlink_alloc_skb error\n");
+        return -1;
     }
 
-    void kernel_receive(struct sk_buff *__skb) //内核从用户空间接收数据
+    nlh = nlmsg_put(nl_skb, 0, 0, USER_MSG, len, 0);
+    if(nlh == NULL)
     {
-        struct sk_buff *skb;
-        struct nlmsghdr *nlh = NULL;
+        printk("nlmsg_put() error\n");
+        nlmsg_free(nl_skb);
+        return -1;
+    }
+    memcpy(nlmsg_data(nlh), pbuf, len);
 
-        char *data = "This is eric's test message from kernel";
+    ret = netlink_unicast(netlinkfd, nl_skb, USER_PORT, MSG_DONTWAIT);
 
-        printk(KERN_DEBUG "[kernel space] begin kernel_receive\n");
-        skb = skb_get(__skb);
+    return ret;
+}
 
-        if(skb->len >= sizeof(struct nlmsghdr)){
-            nlh = (struct nlmsghdr *)skb->data;
-            if((nlh->nlmsg_len >= sizeof(struct nlmsghdr))
-                && (__skb->len >= nlh->nlmsg_len)){
-                user_process.pid = nlh->nlmsg_pid;
-                printk(KERN_DEBUG "[kernel space] data receive from user are:%s\n", (char *)NLMSG_DATA(nlh));
-                printk(KERN_DEBUG "[kernel space] user_pid:%d\n", user_process.pid);
-                send_to_user(data);
-            }
-        }else{
-            printk(KERN_DEBUG "[kernel space] data receive from user are:%s\n",(char *)NLMSG_DATA(nlmsg_hdr(__skb)));
-            send_to_user(data);
+
+static void recv_cb(struct sk_buff *skb)
+{
+    struct nlmsghdr *nlh = NULL;
+    void *data = NULL;
+
+    printk("skb->len:%u\n", skb->len);
+    if(skb->len >= nlmsg_total_size(0))
+    {
+        nlh = nlmsg_hdr(skb);
+        data = NLMSG_DATA(nlh);
+        if(data)
+        {
+            printk("kernel receive data: %s\n", (int8_t *)data);
+            send_msg(data, nlmsg_len(nlh));
         }
-
-        kfree_skb(skb);
     }
+} 
 
-    int __init test_netlink_init(void)
+
+struct netlink_kernel_cfg cfg = 
+{
+    .input = recv_cb,
+};
+
+static int __init test_netlink_init(void)
+{
+    printk("init netlink_demo!\n");
+
+
+    netlinkfd = netlink_kernel_create(&init_net, USER_MSG, &cfg);
+    if(!netlinkfd)
     {
-
-        netlinkfd = netlink_kernel_create(&init_net, NETLINK_TEST, 0, kernel_receive, NULL, THIS_MODULE);
-        if(!netlinkfd){
-            printk(KERN_ERR "can not create a netlink socket\n");
-            return -1;
-        }
-        return 0;
+        printk(KERN_ERR "can not create a netlink socket!\n");
+        return -1;
     }
 
-    void __exit test_netlink_exit(void)
-    {
-        sock_release(netlinkfd->sk_socket);
-        printk(KERN_DEBUG "test_netlink_exit!!\n");
-    }
+    printk("netlink demo init ok!\n");
+    return 0;
+}
 
-    module_init(test_netlink_init);
-    module_exit(test_netlink_exit);
-    MODULE_LICENSE("GPL");
-    MODULE_AUTHOR("eric.hu");
+static void __exit test_netlink_exit(void)
+{
+    sock_release(netlinkfd->sk_socket);
+    printk(KERN_DEBUG "netlink exit\n!");
+}
+
+module_init(test_netlink_init);
+module_exit(test_netlink_exit);
